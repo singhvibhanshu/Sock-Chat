82e944d5bda8aa57d863d195937209d449e36d9c
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-09-08 11:32:24 +0530

    initial fiels resday

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import React = require("react");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+
+wss.on("connection", (socket) => {
+    userCount = userCount + 1;
+    console.log("user connected #" + userCount);
+})
