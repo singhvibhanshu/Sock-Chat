9db9317ef53d328b572568c65a21a0b4e1e2c3d7
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-09-18 12:09:18 +0530

    backend created with some minor bugs

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,38 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let usersCount = 0;
-
 wss.on("connection", (socket) => {
-    usersCount += 1;
-    console.log("user connected #" + usersCount);
 
     socket.on("message", (message) => {
-        console.log("message received: " + message);
-    })
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            console.log("user joined room" + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                roomId: parsedMessage.payload.roomId
+            })
+        }
+
+        if (parsedMessage.type === "chat") {
+            console.log("user wants to chat");
+
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket === socket) {
+                    currentUserRoom = allSockets[i].roomId;
+                
+                }
+            })
+        }
+
+        for (let i = 0; i < allSockets.length; i++) {
+            if(allSockets[i].roomId === currentUserRoom && allSockets[i].socket !== socket) {
+                allSockets[i].socket.send(JSON.stringify({
+                    type: "chat",
+                    payload: {
+                        message: parsedMessage.payload.message
+                    }
+                }))
+            }
+    }
 })
